a0c6bda5af1a4d9ce08b93b5c147bc0af8c9b44f
sribatsha dash <78023279+srivtx@users.noreply.github.com>
2026-04-04 21:57:07 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        // valid anagram 
+        let result = [];
+        let visited = new Array(strs.length).fill(false); 
+
+        for (let i = 0 ; i < strs.length ; i++){
+            if (visited[i]) continue ; 
+            let group = [] ; 
+            let key1 =strs[i].split('').sort().join(''); 
+            group.push(strs[i]); 
+            visited[i] = true ; 
+            for(let j = i + 1 ; j < strs.length ; j++){
+                if(visited[j]) continue ; 
+                let key2 = strs[j].split('').sort().join(''); 
+                if ( key1 === key2 ){
+                    group.push(strs[j]); 
+                    visited[j] = true ; 
+                }
+            }
+               result.push(group); 
+    }return result ; 
+}
+}
