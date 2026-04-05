9a6da0c4373378c5dd44715634e48bbfe90d631f
sribatsha dash <78023279+srivtx@users.noreply.github.com>
2026-04-05 19:00:17 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+
+        let map = new Map(); 
+        let max = -Infinity ; 
+        
+        for ( let i = 0 ; i < nums.length ; i++){
+            if (map.has(nums[i])){
+                map.set(nums[i] , map.get(nums[i]) + 1); 
+            }else {
+                map.set(nums[i] , 1); 
+            }
+        }
+
+        let maxkey = null ; 
+        let maxValue = -Infinity ; 
+        for (let [key , value ] of map){
+            if (value > maxValue){
+                maxkey =  key ; 
+                maxValue = value 
+            }
+        }
+        return maxkey ; 
+    }
+}
