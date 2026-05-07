782b11e82be52a2c494f88f75fc7770fd3fa473d
sribatsha dash <78023279+srivtx@users.noreply.github.com>
2026-05-07 19:04:54 +0530

    Add: search-in-rotated-sorted-array-ii - submission-1

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-1.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {boolean}
+     */
+    search(nums, target) {
+        let left  = 0 ; 
+        let right  = nums.length  - 1  ; 
+        while( left <= right){
+            let mid = Math.floor((left  + right) / 2)  ; 
+            if (nums[mid] === target ){
+                return true ; 
+            }
+            if ( nums[left] === nums [mid] && nums[mid] === nums[right]){
+                left++ ; 
+                right-- ; 
+            }else if ( nums[left] <= nums[mid]){
+                if(target < nums[mid] && target >= nums[left]){
+                    right = mid - 1 ; 
+                }else {
+                    left = mid + 1 ;
+                }
+            }else {
+                if (target > nums[mid] && target <= nums[right]){
+                    left = mid + 1 ; 
+                }else {
+                    right = mid - 1 ;
+                }
+            }
+        }
+        return false ;
+    }
+}
